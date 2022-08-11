fb36633ee06db8a5eccccd2031a5d929c33dcfbf
walss243 <stephen.walsh243@mail.dcu.ie>
2022-08-12 00:20:33 +0100

    adding improved version of fib.py

---
+++ Random/fib_new.py
@@ -0,0 +1,64 @@
+
+
+def big_numbers():
+    d = {                  "Units": 1,                            "Tens": 10,                        "Hundreds": 100,
+                       "Thousands": 10**3,                    "Millions": 10**6,                     "Billions": 10**9,                     "Trillions": 10**12,                     "Quadrillions": 10**15,                 "Quintillions": 10**18,                 "Sextillions": 10**21,                    "Septillions": 10**24,                   "Octillions": 10**27,                    "Nonillions": 10**30,
+                      "Decillions": 10**33,               "Undecillions": 10**36,               "Duodecillions": 10**39,                "Tredecillions": 10**42,               "Quattuordecillions": 10**45,               "Quindecillions": 10**48,               "Sexdecillions": 10**51,               "Septendecillions": 10**54,               "Octodecillions": 10**57,               "Novemdecillions": 10**60,
+                   "Vigintillions": 10**63,            "Unvigintillions": 10**66,            "Duovigintillions": 10**69,            "Tresvigintillions": 10**72,            "Quattuorvigintillions": 10**75,            "Quinvigintillions": 10**78,            "Sexvigintillions": 10**81,            "Septemvigintillions": 10**84,            "Octovigintillions": 10**87,            "Novemvigintillions": 10**90,
+                  "Trigintillions": 10**93,           "Untrigintillions": 10**96,           "Duotrigintillions": 10**99,           "Trestrigintillions": 10**102,          "Quattuortrigintillions": 10**105,          "Quintrigintillions": 10**108,          "Sextrigintillions": 10**111,          "Septentrigintillions": 10**114,          "Octotrigintillions": 10**117,          "Novemtrigintillions": 10**120,
+               "Quadragintillions": 10**123,       "Unquadragintillions": 10**126,       "Duoquadragintillions": 10**129,        "Trequadragintillions": 10**132,       "Quattuorquadragintillions": 10**135,       "Quinquadragintillions": 10**138,       "Sexquadragintillions": 10**141,       "Septenquadragintillions": 10**144,       "Octoquadragintillions": 10**147,       "Novemquadragintillions": 10**150,
+              "Quinquagintillions": 10**153,      "Unquinquagintillions": 10**156,      "Duoquinquagintillions": 10**159,       "Trequinquagintillions": 10**162,      "Quattuorquinquagintillions": 10**165,      "Quinquinquagintillions": 10**168,      "Sexquinquagintillions": 10**171,      "Septenquinquagintillions": 10**174,      "Octoquinquagintillions": 10**177,      "Novemquinquagintillions": 10**180,
+                 "Sexagintillions": 10**183,         "Unsexagintillions": 10**186,         "Duosexagintillions": 10**189,          "Tresexagintillions": 10**192,         "Quattuorsexagintillions": 10**195,         "Quinsexagintillions": 10**198,         "Sexsexagintillions": 10**201,         "Septensexagintillions": 10**204,         "Octosexagintillions": 10**207,         "Novemsexagintillions": 10**210,
+               "Septuagintillions": 10**213,       "Unseptuagintillions": 10**216,       "Duoseptuagintillions": 10**219,        "Treseptuagintillions": 10**222,       "Quattuorseptuagintillions": 10**225,       "Quinseptuagintillions": 10**228,       "Sexseptuagintillions": 10**231,       "Septenseptuagintillions": 10**234,       "Octoseptuagintillions": 10**237,       "Novemseptuagintillions": 10**240,
+                 "Octogintillions": 10**243,         "Unoctogintillions": 10**246,         "Duooctogintillions": 10**249,          "Treoctogintillions": 10**252,         "Quattuoroctogintillions": 10**255,         "Quinoctogintillions": 10**258,         "Sexoctogintillions": 10**261,         "Septenoctogintillions": 10**264,         "Octooctogintillions": 10**267,         "Novemoctogintillions": 10**270,
+                 "Nonagintillions": 10**273,         "Unnonagintillions": 10**276,         "Duononagintillions": 10**279,          "Trenonagintillions": 10**282,         "Quattuornonagintillions": 10**285,         "Quinnonagintillions": 10**288,         "Sexnonagintillions": 10**291,         "Septennonagintillions": 10**294,         "Octononagintillions": 10**297,         "Novemnonagintillions": 10**300,
+                      "Centillion": 10**303,              "Uncentillion": 10**306,             "Duocentillions": 10**309,              "Trecentillions": 10**312,             "Quattuorcentillions": 10**315,             "Quincentillions": 10**318,             "Sexcentillions": 10**321,             "Septencentillions": 10**324,             "Octocentillions": 10**327,             "Novemcentillions": 10**330,
+                 "Decicentillions": 10**333,         "Undecicentillions": 10**336,         "Duodecicentillions": 10**339,          "Tredecicentillions": 10**342,         "Quattourdecicentillions": 10**345,         "Quindecicentillions": 10**348,         "Sexdecicentillions": 10**351,         "Septendecicentillions": 10**354,         "Octodecicentillions": 10**357,         "Novemdecicentillions": 10**360,
+              "Viginticentillions": 10**363,      "Unviginticentillions": 10**366,      "Duoviginticentillions": 10**369,      "Tresviginticentillions": 10**372,      "Quattuorviginticentillions": 10**375,      "Quinviginticentillions": 10**378,      "Sexviginticentillions": 10**381,      "Septenviginticentillions": 10**384,      "Octoviginticentillions": 10**387,      "Novemviginticentillions": 10**390,
+             "Trigintacentillions": 10**393,     "Untrigintacentillions": 10**396,     "Duotrigintacentillions": 10**399,     "Trestrigintacentillions": 10**402,     "Quattuortrigintacentillions": 10**405,     "Quintrigintacentillions": 10**408,     "Sextrigintacentillions": 10**411,     "Septentrigintacentillions": 10**414,     "Octotrigintacentillions": 10**417,     "Novemtrigintacentillions": 10**420,
+          "Quadragintacentillions": 10**423,  "Unquadragintacentillions": 10**426,  "Duoquadragintacentillions": 10**429,  "Tresquadragintacentillions": 10**432,  "Quattuorquadragintacentillions": 10**435,  "Quinquadragintacentillions": 10**438,  "Sexquadragintacentillions": 10**441,  "Septenquadragintacentillions": 10**444,  "Octoquadragintacentillions": 10**447,  "Novemquadragintacentillions": 10**450,
+         "Quinquagintacentillions": 10**453, "Unquinquagintacentillions": 10**456, "Duoquinquagintacentillions": 10**459, "Tresquinquagintacentillions": 10**462, "Quattuorquinquagintacentillions": 10**465, "Quinquinquagintacentillions": 10**468, "Sexquinquagintacentillions": 10**471, "Septenquinquagintacentillions": 10**474, "Octoquinquagintacentillions": 10**477, "Novemquinquagintacentillions": 10**480,
+            "Sexagintacentillions": 10**483,    "Unsexagintacentillions": 10**486,    "Duosexagintacentillions": 10**489,     "Tresexagintacentillions": 10**492,    "Quattuorsexagintacentillions": 10**495,    "Quinsexagintacentillions": 10**498,    "Sexsexagintacentillions": 10**501,    "Septensexagintacentillions": 10**504,    "Octosexagintacentillions": 10**507,    "Novemsexagintacentillions": 10**510,
+          "Septuagintacentillions": 10**513,  "Unseptuagintacentillions": 10**516,  "Duoseptuagintacentillions": 10**519,   "Treseptuagintacentillions": 10**522,  "Quattuorseptuagintacentillions": 10**525,  "Quinseptuagintacentillions": 10**528,  "Sexseptuagintacentillions": 10**531,  "Septenseptuagintacentillions": 10**534,  "Octoseptuagintacentillions": 10**537,  "Novemseptuagintacentillions": 10**540,
+            "Octogintacentillions": 10**543,    "Unoctogintacentillions": 10**546,    "Duooctogintacentillions": 10**549,     "Treoctogintacentillions": 10**552,    "Quattuoroctogintacentillions": 10**555,    "Quinoctogintacentillions": 10**558,    "Sexoctogintacentillions": 10**561,    "Septenoctogintacentillions": 10**564,    "Octooctogintacentillions": 10**567,    "Novemoctogintacentillions": 10**570,
+            "Nonagintacentillions": 10**573,    "Unnonagintacentillions": 10**576,    "Duononagintacentillions": 10**579,     "Trenonagintacentillions": 10**582,    "Quattuornonagintacentillions": 10**585,    "Quinnonagintacentillions": 10**588,    "Sexnonagintacentillions": 10**591,    "Septennonagintacentillions": 10**594,    "Octononagintacentillions": 10**597,    "Novemnonagintacentillions": 10**600,
+                    "Ducentillons": 10**603,            "Unducentillons": 10**606,            "Duoducentillons": 10**609,             "Treducentillons": 10**612,            "Quattuorducentillons": 10**615,            "Quinducentillons": 10**618,            "Sexducentillons": 10**621,            "Septenducentillons": 10**624,            "Octoducentillons": 10**627,            "Novemducentillons": 10**630, "Unknown": 10**633
+         }
+    return d
+
+
+def main():
+    """ Outputs up to the 3030th Fibonacci number before reading unknown. """
+    white = "\u001b[37m"
+    green = "\u001b[32m"
+    yellow = "\u001b[33;1m"
+    prev = 0
+    curr = 1
+    i = 0
+
+    num = int(input(" Enter Number: "))
+    assert num > 0, "Enter Positive Integer Greater Than 0"
+
+    print("\n")
+    for i in range(0, num):
+
+        d = big_numbers()
+
+        for k, v in d.items():
+            if curr >= v:
+                num_name = k
+
+        title = ' Fibonacci Number {}. Located in the {}{}'.format(i + 1, green, num_name)
+        fib_num = '{} {:,}{}\n'.format(yellow, curr, white)
+
+        new_num = prev + curr
+        prev = curr
+        curr = new_num
+        i += 1
+
+    print(title)
+    print(fib_num)
+
+
+if __name__ == "__main__":
+    main()
